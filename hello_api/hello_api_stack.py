c761e1055ea4f228e94b6c537cfa46eb57c1a3a3
Vadym Vlasenko <vadym_vlasenko@epam.com>
2024-09-17 20:10:32 +0300

    fix cdk

--- hello_api/hello_api_stack.py
+++ hello_api/hello_api_stack.py
@@ -1,26 +1,11 @@
-import os
-
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
-    Environment,
-    IAspect,
-    Aspects
+    Environment
 )
-from aws_cdk import aws_iam as iam
-from constructs import Construct, IConstruct
-
-
-class PermissionBoundaryAspect(IAspect):
-    def __init__(self, permission_boundary_arn: str):
-        self.permission_boundary_arn = permission_boundary_arn
-
-    def visit(self, node: IConstruct):
-        if isinstance(node, iam.Role):
-            node.add_permission_boundary(iam.ManagedPolicy.from_managed_policy_arn(
-                node, "PermissionBoundary", self.permission_boundary_arn))
-
+import os
+from constructs import Construct
 
 class HelloApiStack(Stack):
 
@@ -30,8 +15,7 @@ class HelloApiStack(Stack):
             region=os.environ["CDK_DEFAULT_REGION"]
         )
         super().__init__(scope, id, env=env, **kwargs)
-        permission_boundary_arn = "arn:aws:iam::025066278959:policy/eo_role_boundary"
-        Aspects.of(self).add(PermissionBoundaryAspect(permission_boundary_arn))
+
         # Define the Lambda function
         hello_lambda = _lambda.Function(
             self, 'HelloHandler',
